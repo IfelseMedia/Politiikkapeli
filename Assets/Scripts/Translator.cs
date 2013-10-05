9ec5f4be11b6cc053a6d21dcde8770dbf3b5a700
Timo Moisio <timohmoisio@gmail.com>
2013-10-05 14:28:19 +0300

    Add dummy translator class

---
+++ Assets/Scripts/Translator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PoliticsGame
+{
+	/// <summary>
+	/// Dummy class for localization.
+	/// TODO: implement actual localization.
+	/// </summary>
+	public class Translator
+	{
+		/// <summary>
+		/// Translates the text into the current language.
+		/// Currently returns the text as such.
+		/// </summary>
+		public string Get(string text, params string[] tokens)
+		{
+			return string.Format(text, tokens);
+		}
+	}
+}
